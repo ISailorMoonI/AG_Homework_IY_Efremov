b70f56d98709e2f4ac3526d6b6bd38e0de5a1087
SailorMoon <yashamaru99@gmail.com>
2021-02-21 18:52:19 +0300

    Completed HW1

---
+++ Homework/Lesson 1 Task 1/AG_HW/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace AG_HW
+{ 
+ class Program
+{ 
+    
+        public static string OddCheck(int n)
+    {
+        string result;
+        var d = 0;
+        var i = 2;
+        while (i < n)
+        {
+            if (n % i == 0)
+            {
+                d++;
+                i++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        if (d == 0)
+        {
+            result = "Простое";
+        }
+        else
+        {
+            result = "Не простое";
+        }
+        return result;
+    }
+
+    public class TestCase
+    {
+        public int input { get; set; }
+        public string expected { get; set; }
+    }
+    static void Main(string[] args)
+    {
+        var testData = new TestCase[5];
+        testData[0] = new TestCase()
+        {
+            input = 1,
+            expected = "Простое"
+        };
+
+        testData[1] = new TestCase()
+        {
+            input = 3,
+            expected = "Простое"
+        };
+
+        testData[2] = new TestCase()
+        {
+            input = 141,
+            expected = "Не простое"
+        };
+        ;
+        testData[3] = new TestCase()
+        {
+            input = 19,
+            expected = "Простое"
+        };
+
+        testData[4] = new TestCase()
+        {
+            input = 6,
+            expected = "Не простое"
+        };
+
+
+        foreach (var TestCase in testData)
+        {
+            var result = OddCheck(TestCase.input);
+            Console.WriteLine($"{TestCase.input} Actual {result}, Expected {TestCase.expected}");
+        }
+
+
+    }
+}
+}
+
---
+++ Homework/Lesson 1 Task 1/Lesson 1 Task 3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Lesson_1_Task_3
+{
+    class Program
+    {
+        static int FibRec(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return FibRec(n - 1) + FibRec(n - 2);
+            }
+        }
+
+        static int FibCycle(int n)
+        {
+            int a = 0;
+            int b = 1;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                tmp = a;
+                a = b;
+                b += tmp;
+            }
+
+            return a;
+        }
+
+        public class TestCase
+        {
+            public int input { get; set; }
+            public string expected { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            var testData = new TestCase[5];
+            testData[0] = new TestCase()
+            {
+                input = 7,
+                expected = "13"
+            };
+
+            testData[1] = new TestCase()
+            {
+                input = 9,
+                expected = "34"
+            };
+
+            testData[2] = new TestCase()
+            {
+                input = 4,
+                expected = "3"
+            };
+            ;
+            testData[3] = new TestCase()
+            {
+                input = 10,
+                expected = "55"
+            };
+
+            testData[4] = new TestCase()
+            {
+                input = 1,
+                expected = "1"
+            };
+            foreach (var TestCase in testData)
+            {
+                var result = FibRec(TestCase.input);
+                Console.WriteLine($"{TestCase.input} Actual {result}, Expected {TestCase.expected}\n");
+            }
+            foreach (var TestCase in testData)
+            {
+                var result = FibCycle(TestCase.input);
+                Console.WriteLine($"{TestCase.input} Actual {result}, Expected {TestCase.expected}\n");
+            }
+        }
+    }
+}
