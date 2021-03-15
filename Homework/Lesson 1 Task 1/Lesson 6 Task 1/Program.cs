25cfce4b40f30e0e0d23ed22db6dc6b2e770a908
SailorMoon <yashamaru99@gmail.com>
2021-03-15 03:38:13 +0300

    bfs dfs

--- Homework/Lesson 1 Task 1/Lesson 6 Task 1/Program.cs
+++ Homework/Lesson 1 Task 1/Lesson 6 Task 1/Program.cs
@@ -1,152 +1,106 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lesson_6_Task_1
 {
-    public class Node
+    class clsGraph
     {
-        public string Name { get; set; }
-        public List<Node> Children { get; }
+        private int Vertices;
+        private List<Int32>[] adj;
 
-        public Node(string name)
+        public clsGraph(int v)
         {
-            Name = name;
-            Children = new List<Node>();
+            Vertices = v;
+            adj = new List<Int32>[v];
+            for (int i = 0; i < v; i++)
+            {
+                adj[i] = new List<Int32>();
+            }
         }
-
-        public Node AddChildren(Node node, bool bidirect = true)
+        public void AddEdge(int v, int w)
         {
-            Children.Add(node);
-            if (bidirect)
-            {
-                node.Children.Add(this);
-            }
-            return this;
+            adj[v].Add(w);
         }
-
-
-
-    }
-    public class DepthFirstSearch
-    {
-        private HashSet<Node> visited;
-        private LinkedList<Node> path;
-        private Node goal;
-
-        public LinkedList<Node> DFS(Node start, Node goal)
+        void BFS(int s)
         {
-            visited = new HashSet<Node>();
-            path = new LinkedList<Node>();
-            this.goal = goal;
-            DFS(start);
-            if (path.Count > 0)
+            bool[] visited = new bool[Vertices];
+            Queue<int> queue = new Queue<int>();
+            visited[s] = true;
+            queue.Enqueue(s);
+            while (queue.Count != 0)
             {
-                path.AddFirst(start);
-            }
-            return path;
-        }
-        private bool DFS(Node node)
-        {
-            if (node == goal)
-            {
-                return true;
-            }
-            visited.Add(node);
-            foreach (var child in node.Children.Where(X => !visited.Contains(X)))
-            {
-                if (DFS(child))
+                s = queue.Dequeue();
+                Console.WriteLine("next->" + s);
+                foreach (Int32 next in adj[s])
                 {
-                    path.AddFirst(child);
-                    return true;
-                }
-            }
-            return false;
-        }
-        public class BreadthFirstSearch
-        {
-            private HashSet<Node> visited;
-            private LinkedList<Node> path;
-            private Node goal;
-
-            public LinkedList<Node> BFS(Node start, Node goal)
-            {
-                visited = new HashSet<Node>();
-                path = new LinkedList<Node>();
-                this.goal = goal;
-                BFS(start);
-                if (path.Count > 0)
-                {
-                    path.AddFirst(start);
-                }
-                return path;
-            }
-            private bool BFS(Node node)
-            {
-                if (node == goal)
-                {
-                    return true;
-                }
-                visited.Add(node);
-                foreach (var child in node.Children.Where(X => !visited.Contains(X)))
-                {
-                    if (BFS(child))
+                    if (!visited[next])
                     {
-                        path.AddFirst(child);
-                        return true;
+                        visited[next] = true;
+                        queue.Enqueue(next);
                     }
                 }
-                return false;
+            }
+        }
+        public void DFS(int s)
+        {
+            bool[] visited = new bool[Vertices];
+            Stack<int> stack = new Stack<int>();
+            visited[s] = true;
+            stack.Push(s);
+            while (stack.Count != 0)
+            {
+                s = stack.Pop();
+                Console.WriteLine("next->" + s);
+                foreach (int i in adj[s])
+                {
+                    if (!visited[i])
+                    {
+                        visited[i] = true;
+                        stack.Push(i);
+                    }
+                }
+            }
+        }
+
+        public void PrintAdjacecnyMatrix()
+        {
+            for (int i = 0; i < Vertices; i++)
+            {
+                Console.Write(i + ":[");
+                string s = "";
+                foreach (var k in adj[i])
+                {
+                    s = s + (k + ",");
+                }
+                s = s.Substring(0, s.Length - 1);
+                s = s + "]";
+                Console.Write(s);
+                Console.WriteLine();
             }
         }
         class Program
         {
             static void Main(string[] args)
             {
-                var n01 = new Node("01");
-                var n02 = new Node("02");
-                var n03 = new Node("03");
-                var n04 = new Node("04");
-                var n05 = new Node("05");
-                var n06 = new Node("06");
-                var n07 = new Node("07");
-                var n08 = new Node("08");
-                var n09 = new Node("09");
-                var n10 = new Node("10");
-                var n11 = new Node("11");
-                var n12 = new Node("12");
-                var n13 = new Node("13");
-                var n14 = new Node("14");
-                var n15 = new Node("15");
-                n01.AddChildren(n02).AddChildren(n03);
-                n02.AddChildren(n05);
-                n03.AddChildren(n04);
-                n04.AddChildren(n05, false).AddChildren(n10, false).AddChildren(n11, false);
-                n06.AddChildren(n01, false);
-                n07.AddChildren(n03, false).AddChildren(n08);
-                n09.AddChildren(n08).AddChildren(n10);
-                n11.AddChildren(n12).AddChildren(n13);
-                n12.AddChildren(n13);
-                n14.AddChildren(n15);
-                var search = new DepthFirstSearch();
-                var path = search.DFS(n06, n10);
-                PrintPath(path); // 06 -> 01 -> 03 -> 04 -> 10
-                var path2 = search.DFS(n06, n14);
-                PrintPath(path2); // You shall not pass!
-            }
-            private static void PrintPath(LinkedList<Node> path)
-            {
-                Console.WriteLine();
-                if (path.Count == 0)
-                {
-                    Console.WriteLine("You shall not pass!");
-                }
-                else
-                {
-                    Console.WriteLine(string.Join(" -> ", path.Select(x => x.Name)));
-                }
-                Console.Read();
+                clsGraph graph = new clsGraph(4);
+                graph.AddEdge(0, 1);
+                graph.AddEdge(0, 2);
+                graph.AddEdge(1, 2);
+                graph.AddEdge(2, 0);
+                graph.AddEdge(2, 3);
+                graph.AddEdge(3, 3);
+
+                graph.PrintAdjacecnyMatrix();
+
+                Console.WriteLine("BFS traversal starting from vertex 2:");
+                graph.BFS(2);
+                Console.WriteLine("DFS traversal starting from vertex 2:");
+                graph.DFS(2);
             }
         }
     }
 }
+
