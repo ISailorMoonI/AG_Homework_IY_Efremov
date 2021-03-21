eadf9fa0ebe9658afa002da85f90e98c923bb275
SailorMoon <yashamaru99@gmail.com>
2021-03-21 16:17:57 +0300

    Reworked with test

--- Homework/Lesson 1 Task 1/Lesson 6 Task 1/Program.cs
+++ Homework/Lesson 1 Task 1/Lesson 6 Task 1/Program.cs
@@ -85,20 +85,53 @@ namespace Lesson_6_Task_1
         {
             static void Main(string[] args)
             {
-                clsGraph graph = new clsGraph(4);
+                clsGraph graph = new clsGraph(16);
                 graph.AddEdge(0, 1);
                 graph.AddEdge(0, 2);
                 graph.AddEdge(1, 2);
                 graph.AddEdge(2, 0);
                 graph.AddEdge(2, 3);
                 graph.AddEdge(3, 3);
+                graph.AddEdge(4, 2);
+                graph.AddEdge(4, 3);
+                graph.AddEdge(4, 4);
+                graph.AddEdge(5, 4);
+                graph.AddEdge(6, 4);
+                graph.AddEdge(7, 1);
+                graph.AddEdge(8, 0);
+                graph.AddEdge(9, 5);
+                graph.AddEdge(10, 9);
+                graph.AddEdge(11, 2);
+                graph.AddEdge(12, 5);
+                graph.AddEdge(13, 10);
+                graph.AddEdge(14, 10);
+                graph.AddEdge(15, 4);
 
                 graph.PrintAdjacecnyMatrix();
 
-                Console.WriteLine("BFS traversal starting from vertex 2:");
-                graph.BFS(2);
-                Console.WriteLine("DFS traversal starting from vertex 2:");
-                graph.DFS(2);
+                Console.WriteLine("BFS traversal starting from vertex 10:");
+                graph.BFS(10); // 10>9>5>4>2>3>0>1
+                Console.WriteLine("BFS traversal starting from vertex 15:");
+                graph.BFS(15); // 15>4>2>3>0>1
+                Console.WriteLine("BFS traversal starting from vertex 7:");
+                graph.BFS(7); // 7>1>2>0>3
+                Console.WriteLine("BFS traversal starting from vertex 6:");
+                graph.BFS(6); // 6>4>2>3>0>1
+                Console.WriteLine("BFS traversal starting from vertex 8:");
+                graph.BFS(8); // 8>0>1>2>3
+                
+
+
+                Console.WriteLine("DFS traversal starting from vertex 14:");
+                graph.DFS(14); // 14>10>9>5>4>3>2>0>1
+                Console.WriteLine("DFS traversal starting from vertex 9:");
+                graph.DFS(9); // 9>5>4>3>2>0>1
+                Console.WriteLine("DFS traversal starting from vertex 5:");
+                graph.DFS(5); // 5>4>3>2>0>1
+                Console.WriteLine("DFS traversal starting from vertex 13:");
+                graph.DFS(13); // 13>10>9>5>4>3>2>0>1
+                Console.WriteLine("DFS traversal starting from vertex 10:");
+                graph.DFS(10); // 10>9>5>4>3>2>0>1
             }
         }
     }
