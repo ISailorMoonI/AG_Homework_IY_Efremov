0d33a384bc64f1f73c14b19a70dd640720f440c4
Sailor Moon <yashamaru99@gmail.com>
2021-03-24 18:56:49 +0300

    Revert "Reworked"
    This reverts commit 74436bd672bf1a0e1118ea7ab477fb03c40c6319.

--- Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
+++ Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
@@ -2,11 +2,14 @@
 
 namespace Lesson_7_Task_1
 <<<<<<< HEAD
+<<<<<<< HEAD
 {
     class Program
     {
         const int N = 6; 
         const int M = 6;
+=======
+>>>>>>> parent of 74436bd... Reworked
 =======
 >>>>>>> parent of 74436bd... Reworked
 
