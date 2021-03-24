8448de7a21568baf5a3add499843a74010aa88ce
SailorMoon <yashamaru99@gmail.com>
2021-03-24 18:21:05 +0300

    Reworked branch

--- Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
+++ Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
@@ -4,8 +4,8 @@ namespace Lesson_7_Task_1
 {
     class Program
     {
-        const int N = 6; 
-        const int M = 6;
+        const int N = 7; 
+        const int M = 7;
 
         static void Print2(int n, int m, int[,] a)
         {
