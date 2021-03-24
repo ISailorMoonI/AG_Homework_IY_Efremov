a3a680e5164310f1723025568531105e71a73be1
Sailor Moon <yashamaru99@gmail.com>
2021-03-24 18:57:04 +0300

    Revert "Empty"
    This reverts commit 96496caaf8009a2eb894c345137dbe2c259aa934.

--- Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
+++ Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
@@ -3,6 +3,7 @@
 namespace Lesson_7_Task_1
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 {
     class Program
     {
@@ -12,4 +13,41 @@ namespace Lesson_7_Task_1
 >>>>>>> parent of 74436bd... Reworked
 =======
 >>>>>>> parent of 74436bd... Reworked
+=======
+{
+    class Program
+    {
+        const int N = 7;
+        const int M = 7;
+>>>>>>> parent of 96496ca... Empty
 
+        static void Print2(int n, int m, int[,] a)
+        {
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < m; j++)
+                    Console.Write(String.Format("{0,5}", a[i, j]));
+                Console.Write("\r\n");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[,] A = new int[N, M];
+            int i, j;
+            for (j = 0; j < M; j++)
+                A[0, j] = 1; // Первая строка заполнена единицами
+            for (i = 1; i < N; i++)
+            {
+                A[i, 0] = 1;
+                for (j = 1; j < M; j++)
+                    A[i, j] = A[i, j - 1] + A[i - 1, j];
+            }
+
+            Print2(N, M, A);
+
+        }
+    }
+}
