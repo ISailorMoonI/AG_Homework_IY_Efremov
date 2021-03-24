6e97456bb43a41fd736c72f594b568871100eb38
Sailor Moon <yashamaru99@gmail.com>
2021-03-24 18:56:36 +0300

    Revert "Reworked"
    This reverts commit 74436bd672bf1a0e1118ea7ab477fb03c40c6319.

--- Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
+++ Homework/Lesson 1 Task 1/Lesson 7 Task 1/Program.cs
@@ -1,39 +1,12 @@
 ﻿using System;
 
 namespace Lesson_7_Task_1
+<<<<<<< HEAD
 {
     class Program
     {
         const int N = 6; 
         const int M = 6;
+=======
+>>>>>>> parent of 74436bd... Reworked
 
-        static void Print2(int n, int m, int[,] a)
-        {
-            int i, j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < m; j++)
-                    Console.Write(String.Format("{0,5}", a[i, j]));
-                Console.Write("\r\n");
-            }
-        }
-
-
-        static void Main(string[] args)
-        {
-            int[,] A = new int[N, M];
-            int i, j;
-            for (j = 0; j < M; j++)
-                A[0, j] = 1; // Первая строка заполнена единицами
-            for (i = 1; i < N; i++)
-            {
-                A[i, 0] = 1;
-                for (j = 1; j < M; j++)
-                    A[i, j] = A[i, j - 1] + A[i - 1, j];
-            }
-
-            Print2(N, M, A);
-
-        }
-    }
-}
